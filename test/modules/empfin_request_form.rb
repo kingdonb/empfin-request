236dd2a5eb0c16462859f1eb59cfefc27b743b35
Kingdon Barrett <kingdon.b@nd.edu>
2019-10-29 18:24:20 -0400

    refactor

--- test/modules/empfin_request_form.rb
+++ test/modules/empfin_request_form.rb
@@ -7,13 +7,59 @@ module EmpfinRequestForm
     USERNAME = USERNAME_PASSWORD.split(':')[0]
     PASSWORD = USERNAME_PASSWORD.split(':')[1]
 
-    def initialize(username: USERNAME, password: PASSWORD, context:)
-      context.visit 'https://sn.nd.edu'
-      context.find('#okta-signin-username').set(USERNAME)
-      context.find('#okta-signin-password').set(PASSWORD)
+    def initialize(username: USERNAME, password: PASSWORD, ctx:)
+      ctx.visit 'https://sn.nd.edu'
+      ctx.find('#okta-signin-username').set(USERNAME)
+      ctx.find('#okta-signin-password').set(PASSWORD)
 
-      context.find('div.navbar-header', text: 'Service Management', wait: 10)
-      context.visit EMPFIN_REQUEST_URL
+      ctx.find('div.navbar-header',
+               text: 'Service Management', wait: 10)
+      ctx.visit EMPFIN_REQUEST_URL
+    end
+  end
+
+  class Fillout
+    GROUP_ENTRY =
+      'input[id="sys_display.IO:4e621036db043200de73f5161d96196b"]'
+    DESCRIBE_WHAT_WORK =
+      'textarea[id="IO:96252ee6db403200de73f5161d9619c4"]'
+    ASSIGN_USER =
+      'input[id="sys_display.IO:e756cc171b3df7009a56ea866e4bcb49"]'
+    attr_reader :ctx, :iframe
+
+    def initialize(ctx:, iframe:)
+      @ctx = ctx; @iframe = iframe
+      ctx.within_frame(iframe) do
+        ctx.find('#item_table')
+        ctx.find('table td.guide_tray', text: 'Customer Care Request')
+      end
+    end
+
+    def fill_out(work:, group:, user:)
+      describe_what_work.value(work)
+      group_entry.value(group)
+      assign_user.value(user)
+    end
+
+    def submit
+      order_now = ctx.find('button#oi_order_now_button')
+      #KB pending - do not automate this until we have consensus
+      # order_now.click
+      binding.pry
+      ctx.find('span',
+               text: 'Thank you, your request has been submitted')
+      request_link = ctx.find('a#requesturl')
+      request_no = request_link.text
+    end
+
+    def describe_what_work
+      ctx.find(DESCRIBE_WHAT_WORK)
+    end
+    def group_entry
+      ctx.find(GROUP_ENTRY)
+    end
+    def assign_user
+      ctx.find(ASSIGN_USER)
     end
   end
 end
--- test/system/service_nows_test.rb
+++ test/system/service_nows_test.rb
@@ -2,55 +2,25 @@ require "application_system_test_case"
 require "modules/empfin_request_form"
 
 class ServiceNowsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit service_nows_url
-  #
-  #   assert_selector "h1", text: "ServiceNow"
-  # end
+  test 'login and submit a request, then visit the request item' do
+    EmpfinRequestForm::Login.new(ctx: self)
 
-  # adapted from:
-  # https://medium.com/@tjdracz/interactive-debug-sessions-in-rspec-with-capybara-6c4dec2619d3
-  # https://stackoverflow.com/questions/6807268/rails-port-of-testing-environment
-  # https://www.rubydoc.info/github/jnicklas/capybara/Capybara%2FSession:visit
-  # [X] https://stackoverflow.com/a/8332872/661659
-  def interactive_debug_session(log_in_as = nil)
-    # return unless Capybara.current_driver == Capybara.javascript_driver
-    # return unless current_url
-    # login_as(log_in_as, scope: :user) if log_in_as.present?
-    # Launchy.open(current_url)
-    binding.pry
-  end
-
-
-  test 'something' do
-    EmpfinRequestForm::Login.new(context: self)
-
-    ifr = find('main iframe#gsft_main', wait: 10)
-    within_frame(ifr) do
+    # sanity check - did we land on the right form?
+    iframe = find('main iframe#gsft_main', wait: 10)
+    within_frame(iframe) do
       find('#item_table')
       find('table td.guide_tray', text: 'Customer Care Request')
-      group_entry = find('input[id="sys_display.IO:4e621036db043200de73f5161d96196b"]')
-      describe_what_work = find('textarea[id="IO:96252ee6db403200de73f5161d9619c4"]')
-      assign_user = find('input[id="sys_display.IO:e756cc171b3df7009a56ea866e4bcb49"]')
-
-      describe_what_work.set(
-        'KB - This is a Test - Test of long text description - Please perform the following steps (test) Test - KB - TEST ONLY'
-      )
-      group_entry.set(
-        'Employee Finance Solutions'
-      )
-      assign_user.set(
-        'Kingdon Barrett'
-      )
-      order_now = find('button#oi_order_now_button')
-      #KB pending - do not automate this until we have consensus
-      # order_now.click
-      binding.pry
-      find('span', text: 'Thank you, your request has been submitted')
-      request_no = find('a#requesturl').text
-
     end
 
-    # interactive_debug_session
+    f = EmpfinRequestForm::Fillout.
+      new(ctx: self, iframe: iframe)
+
+    f.fill_out(
+      work: 'KB - This is a Test - Test of long text description - Please perform the following steps (test) Test - KB - TEST ONLY',
+      group: 'Employee Finance Solutions',
+      user: 'Kingdon Barrett'
+    )
+
+    ritm_no = f.submit
   end
 end
