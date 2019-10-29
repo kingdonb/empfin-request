2d559e33e1d650359f7eca77d33b0be9402d8b72
Kingdon Barrett <kingdon.b@nd.edu>
2019-10-29 16:26:28 -0400

    do not use the server for testing
    the application is empty, we will just build some Plain Old Ruby Classes
    to drive the tests around our Okta log-in process and Request Form URL

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,10 +4,13 @@ require 'rails/test_help'
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
+  # parallelize(workers: :number_of_processors)
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+  # fixtures :all
+
+  Capybara.app_host = 'https://sn.nd.edu'
+  Capybara.run_server = false # don't start Rack
 
   # Add more helper methods to be used by all tests here...
 end
