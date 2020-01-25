3f654491fc104a0518afe0bff7e8be6a448f0b6e
Kingdon Barrett <kingdon.b@nd.edu>
2020-01-25 13:50:17 -0500

    (red) some passing tests
    Finished in 0.04327 seconds (files took 1.23 seconds to load)
    20 examples, 6 failures, 10 pending
    
    At least we are configured to use guard now!

--- spec/models/concerns/empfin_request_form/main_spec.rb
+++ spec/models/concerns/empfin_request_form/main_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 RSpec.describe EmpfinRequestForm::Main do
   context "holds a context from ApplicationSystemTestCase" do
     describe "#run" do
+      let(:subject) { described_class.
+                      new(ctx: double(:application_system_test_context)) }
+
       it 'organizer interactor' do
         expect(subject).to receive(:do_login)
         expect(subject).to receive(:validate_login_destination)
@@ -13,21 +16,32 @@ RSpec.describe EmpfinRequestForm::Main do
       end
     end
 
-    describe "#do_login" do
-      it 'pending'
-    end
+    context "interactor methods" do
+      let(:ctx) { double(:application_system_test_context) }
+      let(:subject) { described_class.new(ctx: ctx) }
+      describe "#do_login" do
+        let(:login) { double('login') }
+        it 'logs you in' do
+          expect(EmpfinRequestForm::Login).to receive(:new).and_return login
+          subject.send(:do_login)
+        end
+      end
 
-    describe "#validate_login_destination" do
-      it 'pending'
-    end
+      describe "#validate_login_destination" do
+        it 'looks for an item_table and the text Customer Care Request' do
+          expect(ctx).to receive(:find).with('#item_table').ordered
+          expect(ctx).to receive(:find).with('table td.guide_tray', text: 'Customer Care Request').ordered
+          subject.send(:validate_login_destination)
+        end
+      end
 
-    describe "#setup_csv_context" do
-      it 'pending'
-    end
+      describe "#setup_csv_context" do
+        it 'pending'
+      end
 
-    describe "#main_loop" do
-      it 'pending'
+      describe "#main_loop" do
+        it 'pending'
+      end
     end
-
   end
 end
