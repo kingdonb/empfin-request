9b65d383f6e325d73165147114813d135534d554
Kingdon Barrett <kingdon.b@nd.edu>
2020-01-25 15:21:04 -0500

    There should be only one place where constants
    ...where you go to comment or uncomment variables when it's time to
    switch back and forth from test to production and vice versa, so that
    it's harder to forget to change something in one place or another.
    
    Always use the constants defined in the Constants module.

--- spec/models/empfin_request_form/fillout_spec.rb
+++ spec/models/empfin_request_form/fillout_spec.rb
@@ -1,21 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe EmpfinRequestForm::Fillout do
-  context "fill out the form, heavy lifting" do
+  xcontext "fill out the form, heavy lifting" do
     describe 'fill out part one' do
+      it 'pending' do
+        subject.fill_out_1
+      end
     end
     describe 'fill out part two' do
+      it 'pending' do
+        subject.fill_out_2
+      end
     end
-    describe 'submit part one'
-    describe 'submit part two'
+    describe 'submit part one' do
+      it 'pending' do
+        subject.submit_1
+      end
+    end
+    describe 'submit part two' do
+      it 'pending' do
+        subject.submit_2
+      end
+    end
   end
-  context "other utility methods" do
-    it '#follow_req_link'
-    it '#find_ritm_number'
-    it '#find_task_number'
-    it '#describe_what_work'
-    it '#group_entry'
-    it '#assign_user'
-    it '#yes_on_behalf_of'
+  xcontext "other utility methods" do
+    it '#follow_req_link' do
+      subject.follow_req_link
+    end
+    it '#find_ritm_number' do
+      subject.find_ritm_number
+    end
+    it '#find_task_number' do
+      subject.find_task_number
+    end
+    it '#describe_what_work' do
+      subject.describe_what_work
+    end
+    it '#group_entry' do
+      subject.group_entry
+    end
+    it '#assign_user' do
+      subject.assign_user
+    end
+    it '#yes_on_behalf_of' do
+      subject.yes_on_behalf_of
+    end
   end
 end
