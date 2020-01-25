9aa1ee07a4febaf33e5ba62565df5193e056aab3
Kingdon Barrett <kingdon.b@nd.edu>
2020-01-25 17:12:21 -0500

    more elaborate EmpfinRequestForm::Fillout spec

--- spec/models/empfin_request_form/fillout_spec.rb
+++ spec/models/empfin_request_form/fillout_spec.rb
@@ -1,19 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe EmpfinRequestForm::Fillout do
+  let(:ctx) { double('ctx') }
+  let(:subject) {
+    expect(ctx).to receive(:find).with('#item_table').ordered
+    expect(ctx).to receive(:find).with('table td.guide_tray', text: 'Customer Care Request').ordered
+    described_class.new(
+      ctx: ctx, iframe: nil )
+  }
+  before(:each) do
+    expect(described_class).to receive(:new).with(
+      ctx: ctx, iframe: nil
+    ).and_call_original
+    subject
+  end
+
   context "fill out the form, heavy lifting" do
-    let(:ctx) { double('ctx') }
-    let(:subject) {
-      expect(ctx).to receive(:find).with('#item_table').ordered
-      expect(ctx).to receive(:find).with('table td.guide_tray', text: 'Customer Care Request').ordered
-      described_class.new(
-        ctx: ctx, iframe: nil )
-    }
-    before(:each) do
-      expect(described_class).to receive(:new).with(
-        ctx: ctx, iframe: nil
-      ).and_call_original
-    end
     describe 'fill out part one' do
       it 'calls #fill_out_1 via #fill_out_1_with_row' do
         w = double('work')
@@ -68,13 +70,12 @@ RSpec.describe EmpfinRequestForm::Fillout do
       let(:request_link) { double('request_link') }
       let(:req_no) { 'REQ00001' }
       it 'submits after fill_out_1' do
-        subject
         expect(ctx).to receive(:find).with('button#oi_order_now_button').ordered.and_return(order_now)
         expect(order_now).to receive(:click)
         expect(ctx).to receive(:find).with(
           'span', text: 'Thank you, your request has been submitted').ordered.and_return(order_now)
 
-        expect(ctx).to receive(:find).with('a#requesturl').and_return request_link
+        expect(ctx).to receive(:find).with('a#requesturl').ordered.and_return request_link
         expect(request_link).to receive(:text).and_return req_no
         expect(subject.submit_1).to eq req_no
       end
@@ -88,27 +89,62 @@ RSpec.describe EmpfinRequestForm::Fillout do
       end
     end
   end
-  xcontext "other utility methods" do
-    it '#follow_req_link' do
-      subject.follow_req_link
+  context "other utility methods" do
+    describe "get a request number and request url" do
+      let(:req_no) { 'REQ00001' }
+      let(:req_link) { double('request_link') }
+      let(:req_url) { double('request_url') }
+      it '#follow_req_link' do
+        expect(ctx).to receive(:find).with('a#requesturl', text: req_no).ordered.and_return req_link
+        expect(req_link).to receive(:[]).with(:href).and_return req_url
+        expect(req_link).to receive(:click)
+        expect(subject.follow_req_link('REQ00001')).to eq req_url
+      end
     end
-    it '#find_ritm_number' do
-      subject.find_ritm_number
+
+    describe "get a request item link and request item number" do
+      let(:ritm_link) { double('ritm_link') }
+      let(:ritm_url) { double('ritm_url') }
+      let(:ritm_no) { double('ritm_no') }
+      it '#find_ritm_number' do
+        expect(ctx).to receive(:find).with('span.tab_caption_text', text: 'Requested Items').ordered
+        expect(ctx).to receive(:find).with('a.linked.formlink', text: 'RITM00').ordered.and_return ritm_link
+        expect(ritm_link).to receive(:text).and_return ritm_no
+        expect(subject.find_ritm_number).to eq [ritm_link, ritm_no]
+      end
     end
-    it '#find_task_number' do
-      subject.find_task_number
+
+    describe "get a task link and task number" do
+      let(:task_link) { double('task_link') }
+      let(:task_url) { double('task_url') }
+      let(:task_no) { double('task_no') }
+      it '#find_task_number' do
+        expect(ctx).to receive(:find).with('span.tab_caption_text', text: 'Catalog Tasks').ordered
+        expect(ctx).to receive(:find).with('a.linked.formlink', text: 'TASK00').ordered.and_return task_link
+        expect(task_link).to receive(:text).and_return task_no
+        expect(subject.find_task_number).to eq [task_link, task_no]
+      end
     end
-    it '#describe_what_work' do
-      subject.describe_what_work
+
+    describe "fill out the Describe What Work field" do
+      let(:field) { double('field') }
+      let(:value) { double('value') }
+      it '#describe_what_work' do
+        expect(ctx).to receive(:find).with(EmpfinRequestForm::Fillout::DESCRIBE_WHAT_WORK).and_return field
+        expect(field).to receive(:set).with(value)
+        subject.describe_what_work(value)
+      end
     end
-    it '#group_entry' do
-      subject.group_entry
-    end
-    it '#assign_user' do
-      subject.assign_user
-    end
-    it '#yes_on_behalf_of' do
-      subject.yes_on_behalf_of
+    xcontext 'select from a list entry' do
+      it '#group_entry' do
+        subject.group_entry
+      end
+      it '#assign_user' do
+        subject.assign_user
+      end
+      it '#yes_on_behalf_of' do
+        subject.yes_on_behalf_of
+      end
     end
   end
 end
