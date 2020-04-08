9b8a1b5812c616c604c09f833017657948292914
Kingdon Barrett <kingdon.b@nd.edu>
2020-04-08 14:25:56 -0400

    CsvWriter with effectively no changes (same tests)

---
+++ spec/models/concerns/empfin_service_review/csv_writer_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe EmpfinServiceReview::CsvWriter do
+  context "writes to a file 'output-srv-file.csv'" do
+    let(:input_array) { [{a: 'asdf', b: 'zxcv'}] }
+    let(:scratch_filename) { 'scratch-file.csv' }
+    describe '#to_csv' do
+      it 'writes a CSV' do
+        expect(CSV).to receive(:open).with(scratch_filename, 'wb').and_call_original
+        described_class.to_csv(input_array: input_array)
+
+        txt = File.read(scratch_filename)
+        expect(txt).to eq(<<~CSV
+          a,b
+          asdf,zxcv
+          CSV
+        )
+      end
+    end
+  end
+end
