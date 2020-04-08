e07578d5d20fde2af1108cca26cd227d01c495c2
Kingdon Barrett <kingdon.b@nd.edu>
2020-04-08 12:39:10 -0400

    redevelop as a different application
    EmpfinRequestForm is done, but the components can be repurposed and
    reused to solve a similar problem of undifferentiated and redundant
    heavy lifting. The new iteration will be called EmpfinServiceReview

--- test/system/service_nows_test.rb
+++ test/system/service_nows_test.rb
@@ -1,8 +1,16 @@
 require "application_system_test_case"
 
 class ServiceNowsTest < ApplicationSystemTestCase
-  test 'login and submit a request, then visit the request item' do
-    e = EmpfinRequestForm::Main.new(ctx: self)
+  # test 'login and submit a request, then visit the request item' do
+  #   e = EmpfinRequestForm::Main.new(ctx: self)
+  #   e.run
+  # end
+  test 'login and review services, then output a Diff csv' do
+    e = EmpfinServiceReview::Main.new(ctx: self)
     e.run
   end
+  # test 'login and review services, then reverse any Diffs from the csv' do
+  #   e = EmpfinServiceReview::Main.new(ctx: self)
+  #   e.run
+  # end
 end
