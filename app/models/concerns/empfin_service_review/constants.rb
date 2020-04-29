9327ec70b9e5321d7d52433e54bec3b51bb144a3
Kingdon Barrett <kingdon.b@nd.edu>
2020-04-29 08:41:46 -0400

    prod url

--- app/models/concerns/empfin_service_review/constants.rb
+++ app/models/concerns/empfin_service_review/constants.rb
@@ -5,6 +5,11 @@ module EmpfinServiceReview::Constants
   #   '%3Dalphasense%5Eoperational_statusIN1%2C2'.freeze
   # SERVICENOW_SITE_URL = 'https://sn.nd.edu'.freeze
   # SERVICENOW_HOME_PAGE_STRING = "Service Management".freeze
+  # SERVICENOW_REQUEST_FORM_URL = 'https://nd.service-now.com/'\
+  #   'com.glideapp.servicecatalog_cat_item_view.do?v=1&sysparm_id=91f464d11b2fb740c81c64207e4bcbcc'\
+  #   '&sysparm_link_parent=6f417e690f632240a6c322d8b1050e8f'\
+  #   '&sysparm_catalog=742ce428d7211100f2d224837e61036d'\
+  #   '&sysparm_catalog_view=catalog_technical_catalog&sysparm_view=catalog_technical_catalog'.freeze
 
   # Test:
   EMPFIN_REQUEST_URL = 'https://ndtest.service-now.com/'\
